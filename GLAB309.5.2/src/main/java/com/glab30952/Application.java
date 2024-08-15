0860deaf7837adbe27ea3a63ae9d62056335cf9c
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-08-15 10:15:11 -0400

    GLAB to demostrate the use of PostMapping

---
+++ GLAB309.5.2/src/main/java/com/glab30952/Application.java
@@ -0,0 +1,13 @@
+package com.glab30952;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.glab30952")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
